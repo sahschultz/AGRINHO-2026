fec4386da129a93565937e331333d314b048e6d1
sahschultz <sarah.portugal.goncalves@escola.pr.gov.br>
2026-07-02 12:08:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -41,11 +41,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
-            if(entry.isIntersecting){
+            if (entry.isIntersecting) {
                 entry.target.classList.add("show");
             }
         });
+    }, {
+        threshold: 0.2
     });
 
     imagens.forEach(img => observer.observe(img));
+
 });
